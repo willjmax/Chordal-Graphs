3d77b82361ec4aa2c6f017fbb26f596b71d3fab1
will <wmaxwell90@gmail.com>
2019-04-09 00:09:43 -0700

    added chordal completion algorithm

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# chordal
+# Chordal.hs
+A library for Chordal graph algorithms in Haskell using the FGL library.
+All graphs are assumed to be given as an elimination ordering
+We provide implementations of algorithms on Chordal graphs for problems that are generally NP-Complete on general graphs
+
+# Current implemented functions:
+`isChordal :: Gr a b -> Bool`
+Checks if an elimination ordering is perfect
+
+`chordalCompletion :: Gr a () -> Gr a ()`
+Takes an elimination ordering of a graph and returns its chordal completion
+
+`maxClique :: Gr a b -> Int`
+Returns the size of a maximum clique in the graph
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,17 @@
 module Lib
     ( graph1
-    , empty_graph
-    , func
     , grph
     , maxClique
     , isChordal
     , triangle
     , chordal1
+    , chordalCompletion
     ) where
 
 import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.PatriciaTree
+
+type TreeDecomp = Gr [Node] ()
 
 cxt  = ([],2,(),[]) :: Context () ()
 grph = cxt & empty :: Gr () ()
@@ -27,6 +28,7 @@ isChordal g = go g 1
                               e = edges g
                     (Nothing, g') -> error "Vertex not found"
 
+-- helper function for isChordal
 isChordal' :: [Node] -> [Edge] -> Bool
 isChordal' [] _  = True
 isChordal' (n:ns) es = vChordal n ns && isChordal' ns es
@@ -34,6 +36,26 @@ isChordal' (n:ns) es = vChordal n ns && isChordal' ns es
         vChordal n [] = True
         vChordal n (n':ns) = (n, n') `elem` es && vChordal n ns
         
+chordalCompletion :: Gr a () -> Gr a ()
+chordalCompletion g = go g 1
+    where
+        go g n
+            | isEmpty g = g
+            | otherwise =
+                case match n g of
+                    (Just c, _) -> go gc (n+1)
+                        where ns = map fst $ lsuc' c
+                              gc = chordalCompletion' g ns
+                    (Nothing, _) -> g
+
+-- helper function for chordalCompletion
+chordalCompletion' :: Gr a () -> [Node] -> Gr a ()
+chordalCompletion' g []     = g
+chordalCompletion' g (n:ns) = chordalCompletion' gc ns
+    where gc = insEdges fs g
+          es = labEdges g
+          fs = [x | x <- zip3 (repeat n) ns (repeat ()), not (x `elem` es)]
+
 maxClique :: Gr a b -> Int
 maxClique g = 1 + go g 1
     where
@@ -49,25 +71,10 @@ chordal1 :: Gr Char ()
 chordal1 = mkGraph [(1, 'a'), (2, 'f'), (3, 'd'), (4, 'h'), (5, 'c'), (6, 'g'), (7, 'b'), (8, 'e')]
                    [(1, 5, ()), (1, 7, ()), (2, 6, ()), (2, 7, ()), (3, 5, ()), (3, 8, ()), (4, 6, ()), (4, 8, ()), (5, 7, ()), (5, 8, ()), (6, 7, ()), (6, 8, ()), (7, 8, ())]
 
--- a, f, d, h, c, g, b, e
-
-empty_graph :: Gr () () 
-empty_graph = empty
-
 triangle :: Gr () ()
 triangle = mkGraph [(1, ()), (2, ()), (3, ())]
                    [(1, 2, ()), (1, 3, ()), (2, 3, ())]
-                   :: Gr () ()
 
 graph1 = mkGraph [(1, ()), (2, ()), (3, ()), (4, ()), (5, ()), (6, ())] 
                  [(1, 2, ()), (1, 4, ()), (2, 3, ()), (2, 4, ()), (2, 5, ()), (2, 6, ())]
                  :: Gr () ()
-
-func :: Gr () () -> Int
-func g
-    | isEmpty g = 0
-    | otherwise = 1 + func g'
-    where
-        n  = noNodes g    
-        v  = context g n
-        g' = delNode n g
